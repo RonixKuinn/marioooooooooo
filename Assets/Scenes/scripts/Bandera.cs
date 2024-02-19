a067186f5b39c69a797c00f728417a3dba99b06b
RonixKuinn <martukygaca@gmail.com>
2024-02-19 12:31:05 +0100

    monedaybanderahachas

--- Assets/Scenes/scripts/Bandera.cs
+++ Assets/Scenes/scripts/Bandera.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bandera : MonoBehaviour
+
 {
+    public AudioClip lvl1Music;
+    public AudioClip flag;
+
+    AudioSource source;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        source = GetComponent<AudioSource>();
+    }
+
+     void Start()
+    {
+        source.clip = lvl1Music;
+        source.Play();
     }
 
     // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D()
     {
-        
+        source.clip = flag;
+        source.Play();
+
     }
 }
--- Assets/Scenes/scripts/CoinScript.cs
+++ Assets/Scenes/scripts/CoinScript.cs
@@ -7,41 +7,25 @@ public class CoinScript : MonoBehaviour
     public Animator anim;
     private BoxCollider2D boxCollider2D;
 
-    private AudioSource source;
-    public AudioClip coinSound;
+    AudioSource source;
+    public AudioClip coin;
 
-    void Awake()
-    {
-        anim = GetComponent<Animator>();
-        boxCollider2D = GetComponent<BoxCollider2D>();
-    }
-
-    /*void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.ParaChoques.tag == "Player")
-        {
-            Destroy(collision.ParaChoques);
-        }
-    }
-    */
-    
     // Start is called before the first frame update
     void Start()
     {
-        anim.SetBool("IsDancing", true);
         
     }
-
-    // Update is called once per frame
-    void Update()
+     void Awake()
     {
-        
+        source = GetComponent<AudioSource>();
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            source.PlayOneShot(coin);
+            Destroy(gameObject, 0.5f);
+        }
     }
 
-    public void CoinDesapeard()
-    {
-        source.PlayOneShot(coinSound);
-        boxCollider2D.enabled = false;
-        Destroy(gameObject, 0.5f);
-    }
 }
