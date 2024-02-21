86fa7480edc3d9fc5427996b88519cf76f1595e8
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-02-21 11:05:01 +0100

    conmenuprincipappapuuuuuuuu

---
+++ Assets/Scenes/scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadScore();
+    }
+
+    void LoadScore()
+    {
+        scoreText.text = "Puntuacion: " + score.ToString();
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
